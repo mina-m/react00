3cdd5d06e5f62270b07a210b8afb9ba9f9f276dc
mina-m <newgnz17055@gmail.com>
2023-05-14 00:17:11 +0900

    testre追加

---
+++ src/components/testre.jsx
@@ -0,0 +1,10 @@
+const TestReact = (props) => {
+    return (
+        <>
+            <h2>{props.title}</h2>
+            <div>{props.contents}</div>
+        </>
+    );
+}
+
+export default TestReact;
